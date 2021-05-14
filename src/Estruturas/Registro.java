c9081a048d621b0068f845cf2c90b8ce25077596
Kleber Curvelo <kleber.sangalo@hotmail.com>
2021-05-14 15:03:48 -0300

    Modifiquei algumas telas, e já é possível fazer alguns selects

--- src/Estruturas/Registro.java
+++ src/Estruturas/Registro.java
@@ -5,12 +5,13 @@ package Estruturas;
 
 public class Registro {
     
-    private String chaveNota,cnpjEmit,cnpjDest,valor,xml;
+    private String chaveNota,cnpjEmit,cnpjDest,dataEmissao,valor,xml;
 
-    public Registro(String chaveNota, String cnpjEmit, String cnpjDest, String valor,String xml) {
+    public Registro(String chaveNota, String cnpjEmit, String cnpjDest,String dataEmissao, String valor,String xml) {
         this.chaveNota = chaveNota;
         this.cnpjEmit = cnpjEmit;
         this.cnpjDest = cnpjDest;
+        this.dataEmissao = dataEmissao;
         this.valor = valor;
         this.xml = xml;
     }
@@ -26,6 +27,11 @@ public class Registro {
     public String getCnpjDest() {
         return cnpjDest;
     }
+
+    public String getDataEmissao() {
+        return dataEmissao;
+    }
+    
 
     public String getValor() {
         return valor;
--- src/Telas/BaixarRegistrosXML.java
+++ src/Telas/BaixarRegistrosXML.java
@@ -1,4 +1,3 @@
-
 package Telas;
 
 import Estruturas.Registro;
@@ -9,44 +8,16 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
-
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class BaixarRegistrosXML extends javax.swing.JFrame {
-    
+
     private ArrayList<Registro> registros;
 
     public BaixarRegistrosXML(ArrayList<Registro> registros) {
         this.registros = registros;
         initComponents();
-       
-    }
-    
-    public void baixarRegistros(String diretorio) {
-        String[] registrosSelecionados = registrosSelecionados(txtNumeroRegistros.getText());
-        
-        StringBuilder montadorString = new StringBuilder();
-        
-        try {
-            PrintWriter pw = new PrintWriter(new File("C:\\Users\\Kleber\\Documents\\BuscaNota\\teste.xml"));
-            pw.write(registros.get(0).getXml());
-            pw.close();
-            
-//        for(int i = 0; i < registrosSelecionados.length;i++) {
-//            montadorString.replace(0, montadorString.length(), "");
-//            montadorString.append(registros.get((Integer.parseInt(registrosSelecionados[i]))-1).getXml());
-//            try {
-//                PrintWriter pw = new PrintWriter(new File(diretorio + "//" +
-//                        registros.get(Integer.parseInt(registrosSelecionados[i])).getChaveNota() +  ".xml"));
-//                pw.write(montadorString.toString(), 0, (montadorString.length()/2));
-////                pw.write(registros.get((Integer.parseInt(registrosSelecionados[i]))-1).getXml());
-//            } catch (FileNotFoundException ex) {
-//                Logger.getLogger(BaixarRegistrosXML.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//            
-//        }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(BaixarRegistrosXML.class.getName()).log(Level.SEVERE, null, ex);
-        }
 
     }
 
@@ -59,6 +30,7 @@ public class BaixarRegistrosXML extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         txtNumeroRegistros = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -70,12 +42,17 @@ public class BaixarRegistrosXML extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("Tahoma", 3, 12)); // NOI18N
         jLabel2.setText("Exemplo: \"1,2,35,48\"");
 
+        txtNumeroRegistros.setText("Todos");
+
         jButton1.setText("Baixar NFes");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 3, 12)); // NOI18N
+        jLabel3.setText("Caso deseje baixar todos, digite \"Todos\".");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -90,6 +67,7 @@ public class BaixarRegistrosXML extends javax.swing.JFrame {
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jButton1)))
                 .addContainerGap())
+            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 368, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -99,8 +77,10 @@ public class BaixarRegistrosXML extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
                 .addComponent(txtNumeroRegistros, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(28, 28, 28)
                 .addComponent(jButton1)
                 .addContainerGap())
         );
@@ -126,26 +106,77 @@ public class BaixarRegistrosXML extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-          
-        JFileChooser seletorPasta = new JFileChooser();
-        seletorPasta.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        seletorPasta.setVisible(true);
-        int escolha = seletorPasta.showSaveDialog(null);
-        if(escolha == JFileChooser.APPROVE_OPTION) {
-            baixarRegistros(seletorPasta.getSelectedFile().getAbsolutePath());
+        String[] registrosSelecionados;
+        if (txtNumeroRegistros.getText().equalsIgnoreCase("todos")) {
+            registrosSelecionados = new String[this.registros.size()];
+            for (int i = 0; i < this.registros.size(); i++) {
+                registrosSelecionados[i] = Integer.toString((i + 1));
+            }
+        } else {
+            registrosSelecionados = registrosSelecionados(txtNumeroRegistros.getText());
+        }
+        if (validaRegistros(registrosSelecionados)) {
+            JFileChooser seletorPasta = new JFileChooser();
+            seletorPasta.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            seletorPasta.setVisible(true);
+            int escolha = seletorPasta.showSaveDialog(null);
+            if (escolha == JFileChooser.APPROVE_OPTION) {
+                baixarRegistros(seletorPasta.getSelectedFile().getAbsolutePath(), registrosSelecionados);
+                JOptionPane.showMessageDialog(new JFrame(), "Arquivos baixados com sucesso!");
+            }
+        } else {
+            JOptionPane.showMessageDialog(new JFrame(), "Preencha novamente os campos como no exemplo!");
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
-
-    public String[] registrosSelecionados(String txtNumeroRegistros){
+    public String[] registrosSelecionados(String txtNumeroRegistros) {
         return txtNumeroRegistros.split(",");
     }
-    
+
+    public void baixarRegistros(String diretorio, String[] registrosSelecionados) {
+
+        StringBuilder montadorString = new StringBuilder();
+
+        for (int i = 0; i < registrosSelecionados.length; i++) {
+            montadorString.replace(0, montadorString.length(), "");
+            montadorString.append(registros.get((Integer.parseInt(registrosSelecionados[i])) - 1).getXml());
+            try {
+                PrintWriter pw = new PrintWriter(new File(diretorio + "//"
+                        + registros.get(Integer.parseInt(registrosSelecionados[i]) - 1).getChaveNota() + ".xml"));
+                pw.write(registros.get((Integer.parseInt(registrosSelecionados[i])) - 1).getXml());
+                pw.close();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(BaixarRegistrosXML.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+
+    }
+
+    public boolean validaRegistros(String[] registrosSelecionados) {
+        if (registrosSelecionados[0].equalsIgnoreCase("todos")) {
+            return true;
+        }
+
+        try {
+            for (int i = 0; i < registrosSelecionados.length; i++) {
+                Integer.parseInt(registrosSelecionados[i]);
+            }
+
+            return true;
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(new JFrame(), "Por favor, escreva somente números separados por vírgulas no campo de texto.");
+            return false;
+        }
+    }
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField txtNumeroRegistros;
     // End of variables declaration//GEN-END:variables
